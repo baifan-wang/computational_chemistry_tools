ae30ccad12fca4c6e2112b301010d5523d7baa08
baifan-wang <ferlich1984@gmail.com>
2017-06-12 09:53:26 +0200

    Add files via upload

--- Amber/makePLANAR_RST.py
+++ Amber/makePLANAR_RST.py
@@ -19,8 +19,8 @@ def read_input_from_file(res_file):
     for i in range(len(lines)):
         temp = lines[i].split()
         residue_list.append(temp)
-        else:
-            raise TypeError('Incorrect residues in line %d' %i)
+#        else:
+#            raise TypeError('Incorrect residues in line %d' %i)
     return residue_list
 
 def base_pair_split(bp):
@@ -29,10 +29,10 @@ def base_pair_split(bp):
     length = len(bp)
     if length != 2 and length%2 == 0:
         for i in range(1,length,2):
-            if not bp[i].isnumeric():
+            if not bp[i].isdigit():
                 raise TypeError('Incorrect base pair! Input base pair like this: G 1 G 2')
         if length == 4:
-            return bp
+            return [bp]
         else:
             bp.append(bp[0])
             bp.append(bp[1])
@@ -58,12 +58,14 @@ parameters = 'rk2=20.0, rk3=20.0, nstep1=0, nstep2=200000'
 
 def generate_restraint(base_pair):
     '''generate restraint for input base pair'''
+    if len(base_pair)!=4:
+        raise IndexError("base pair must be 4")
     res_key = base_pair[0]+base_pair[2]
     res1 = base_pair[0]+base_pair[1]
     res2 = base_pair[2]+base_pair[3]
     iat = ','.join([base_pair[1],base_pair[1],base_pair[3],base_pair[3]])
 
-    string = (res1, res2, atom_dict[res_key][0], iat, parameters, atom_dict[res_key][1], \
+    s = (res1, res2, atom_dict[res_key][0], iat, parameters, atom_dict[res_key][1], \
      atom_dict[res_key][2], iat, atom_dict[res_key][3], atom_dict[res_key][4], \
      iat, atom_dict[res_key][5], res1, res2)
 
@@ -82,7 +84,7 @@ def generate_restraint(base_pair):
      r1=350.0, r2=355.0, r3=365.0, r4=370.0, iresid=1, &end
 # ----end of planar restraint for base pair %s->%s----\n''' 
 
-    return restraint %string
+    return restraint %s
 
 def write_restraint_file(restraints, output_file):
     try:
@@ -106,8 +108,8 @@ if __name__ == '__main__':
     else:
         for bp in residue_list:
             base_pairs = base_pair_split(bp)
-            for i in base_pairs:
-                restraint_list.append(generate_restraint(i))
+            for b in base_pairs:
+                restraint_list.append(generate_restraint(b))
     if output_file:
         write_restraint_file(restraint_list, output_file)
         print('successfully write the restraint file: %s' %output_file)
