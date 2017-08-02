8c5ca07ae29bef2df61acf4fe66be5881711daa5
baifan-wang <ferlich1984@gmail.com>
2017-08-02 16:57:51 +0200

    Update amd_generate.py

--- Amber/amd_generate.py
+++ Amber/amd_generate.py
@@ -1,5 +1,4 @@
-
-#!/bin/env python3
+#!/usr/bin/env python3
 def file_loader(file):
     """
     load the file and return the lines in this file.
@@ -23,21 +22,38 @@ def get_energy(file):
             DIHE.append(float(line.split()[8]))
     return sum(EPTOT)/len(EPTOT), sum(DIHE)/len(DIHE)
 
-def generate_amd_input(res_num, atm_num, avg_eptot, avg_dihe):
+def generate_amd_input(res_num, atm_num, avg_eptot, avg_dihe, time):
     alphaP   =atm_num*0.2
     EthreshP =avg_eptot+alphaP
     EthreshD =res_num*3.5+avg_dihe
     alphaD   =0.2*3.5*res_num
     print('Input for Accelerated Molecular Dynamics simulation are:')
     print("ethreshd=%.2f, alphad=%.2f,\nethreshp=%.2f, alphap=%.2f" %(EthreshD, alphaD, EthreshP, alphaP))
+    nstlim = int(int(time)/0.000002)
+    s = """amd in vt
+ &cntrl
+  imin = 0, irest = 1, ntx = 5,
+  nstlim = %s, dt = 0.002,
+  ntc = 2, ntf = 2, ig = -1,
+  cut = 8.0, ntb = 1, ntp = 0,
+  ntpr = 1000, ntwx=1000, ntwr = 10000,
+  ntt = 3, gamma_ln = 2.0, temp0 = 300.0,
+  ioutfm = 1, iwrap = 1, ntxo = 2,
+  iamd = 3,
+  ethreshd = %.2f, alphad = %.2f,
+  ethreshp = %.2f, alphap = %.2f
+/
+""" %(nstlim,EthreshD, alphaD, EthreshP, alphaP)
+    with open('amd.in','w') as f:
+        f.write(s)
 
 if __name__ =="__main__":
     import sys
-    mdout, res_num, atm_num = sys.argv[1:]
+    mdout, res_num, atm_num, time = sys.argv[1:]
     res_num = float(res_num)
     atm_num = float(atm_num)
     avg_eptot, avg_dihe = get_energy(mdout)
     print("Energy potential energy is: %.3f kcal/mol" %avg_eptot)
     print("Energy dihedral energy is: %.3f kcal/mol" %avg_dihe)
     print("Number of residue is: %s, number of atoms is: %s" %(res_num, atm_num))
-    generate_amd_input(res_num, atm_num, avg_eptot, avg_dihe)
+    generate_amd_input(res_num, atm_num, avg_eptot, avg_dihe, time)
